44258fbba767b7ec7cd3acb8a56c7ca98b33eac4
minho <dnjsalsgh123@gmail.com>
2023-12-10 07:37:18 +0900

    update: basic if

---
+++ src/cond/CondOp2.java
@@ -0,0 +1,9 @@
+package cond;
+
+public class CondOp2 {
+    public static void main(String[] args) {
+        int age = 19;
+        String state = (age >= 19) ? "성인" : "미성년자";
+        System.out.println(state);
+    }
+}
---
+++ src/cond/CondOpEx.java
@@ -0,0 +1,10 @@
+package cond;
+
+public class CondOpEx {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int better = (a > b) ? a : b;
+        System.out.println(better);
+    }
+}
