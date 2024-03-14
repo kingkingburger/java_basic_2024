108d6ab6fd1a1be551ac2d1dc7555aea14eedda1
minho <dnjsalsgh123@gmail.com>
2024-03-15 05:57:04 +0900

    update: how to working in object change

---
+++ src/basic/ref/VarChang1.java
@@ -0,0 +1,17 @@
+package basic.ref;
+
+public class VarChang1 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = a;
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+        a = 20;
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        b = 30;
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}
---
+++ src/basic/ref/VarChange2.java
@@ -0,0 +1,18 @@
+package basic.ref;
+
+public class VarChange2 {
+    public static void main(String[] args) {
+        Data dataA = new Data();
+        dataA.value = 10;
+        Data dataB = dataA;
+
+        System.out.println("dataA = " + dataA);
+        System.out.println("dataB = " + dataB);
+        System.out.println("dataA = " + dataA.value);
+        System.out.println("dataB = " + dataB.value);
+
+        dataA.value = 20;
+        System.out.println(dataA.value);
+        System.out.println("dataB = " + dataB.value);
+    }
+}
