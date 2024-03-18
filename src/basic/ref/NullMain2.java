468b4b00c052131bf02dc6452f1db952b8fa282b
kingkingburger <dnjsalsgh123@gmail.com>
2024-03-18 18:32:20 +0900

    update: 컴파일 실행안되서 주석처리

--- src/basic/ref/NullMain2.java
+++ src/basic/ref/NullMain2.java
@@ -2,6 +2,6 @@ package basic.ref;
 
 public class NullMain2 {
     Data data = null;
-    data.value = 10;
-    System.out.println("data = " + data.value);
+//    data.value = 10;
+//    System.out.println("data = " + data.value);
 }
