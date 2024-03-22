91d05687e3751cc1f8218aaf690a474d3bb1df1c
kingkingburger <dnjsalsgh123@gmail.com>
2024-03-22 16:18:29 +0900

    update: how to use final

---
+++ src/final1/ConstructInit1.java
@@ -0,0 +1,9 @@
+package final1;
+
+public class ConstructInit1 {
+  final int value;
+
+  public ConstructInit1(int value) {
+    this.value = value;
+  }
+}
---
+++ src/final1/FieldInit.java
@@ -0,0 +1,5 @@
+package final1;
+
+public class FieldInit {
+  static final int SS = 10;
+}
