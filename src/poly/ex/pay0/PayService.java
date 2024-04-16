2a5219428dd93447f0f62af86485444a136ae59f
minho <dnjsalsgh123@gmail.com>
2024-04-16 20:54:27 +0900

    refactoring: check of polymorphism4

--- src/poly/ex/pay0/PayService.java
+++ src/poly/ex/pay0/PayService.java
@@ -2,18 +2,11 @@ package poly.ex.pay0;
 
 public class PayService {
   public void processPay(String option, int amount) {
-    boolean result;
+
     System.out.println("결제를 시작합니다: option=" + option + ", amount=" + amount);
-    if (option.equals("kakao")) {
-      Pay kakaoPay = new KakaoPay();
-      result = kakaoPay.pay(amount);
-    } else if (option.equals("naver")) {
-      Pay naverPay = new NaverPay();
-      result = naverPay.pay(amount);
-    } else {
-      System.out.println("결제 수단이 없습니다.");
-      result = false;
-    }
+
+    Pay pay = PayStore.findPay(option);
+    boolean result = pay.pay(amount);
 
     if (result) {
       System.out.println("결제가 성공했습니다.");
--- src/poly/ex/pay0/PayStore.java
+++ src/poly/ex/pay0/PayStore.java
@@ -1,1 +1,19 @@
+package poly.ex.pay0;
 
+import poly.ex.pay0.DefaultPay;
+import poly.ex.pay0.KakaoPay;
+import poly.ex.pay0.NaverPay;
+import poly.ex.pay0.Pay;
+
+public abstract class PayStore {
+
+  public static Pay findPay(String option) {
+    if (option.equals("kakao")) {
+      return new KakaoPay();
+    } else if (option.equals("naver")) {
+      return new NaverPay();
+    } else {
+      return new DefaultPay();
+    }
+  }
+}
